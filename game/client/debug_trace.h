5e19961acf866b3efcf1be7923fd385044d438c0
meowcat767 <meowcat@meowcat.site>
2026-06-18 10:21:47 +0100

    create header for the trace debugger

---
+++ game/client/debug_trace.h
@@ -0,0 +1,15 @@
+// (C) Valve Software, meowcat767 & MIXX Brigade 2026
+// --------------------------------------------------
+// Purpose: header for the trace debugger
+// --------------------------------------------------
+
+#ifndef DEBUG_TRACE_H
+#define DEBUG_TRACE_H
+
+#ifdef _WIN32
+#pragma once
+#endif
+
+void DebugTrace_Update();
+
+#endif
